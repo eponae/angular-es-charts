bf987939cffec0459b9362af53a1f31d76c2511b
lili1725 <alice.rimassa@gmail.com>
2016-08-21 21:58:31 +0200

    Added drilldown on chart

--- src/modules/charts/controllers/ChartsCtrl.js
+++ src/modules/charts/controllers/ChartsCtrl.js
@@ -1,15 +1,16 @@
 'use strict';
 
-angular.module('basics').controller('ChartsCtrl', ['$scope', 'esServ', function ($scope, esServ) {
+angular.module('basics').controller('ChartsCtrl', ['$scope', 'esServ', '$timeout', function ($scope, esServ, $timeout) {
 
         $scope.type = 'pie';
         $scope.dep = 0;
 
         $scope.chooseDep = "Select a department";
         $scope.deps = [];
+        $scope.title = "Departments";
 
         var loadDeps = function (fieldName) {
-            var request = 'conservatory_index/conservatories/_search?source={"size":0,"aggs":{"_res":{"terms":{"field":"' + fieldName + '","size":0}}}}&pretty=true';
+            var request = 'conservatory_index/conservatories/_search?source={"aggs":{"_res":{"terms":{"field":"' + fieldName + '"}}}}&pretty=true';
 
             esServ.getData(request, {
                 then: function (response) {
@@ -23,14 +24,70 @@ angular.module('basics').controller('ChartsCtrl', ['$scope', 'esServ', function 
                 }
             });
         };
-        
-        var formatDeps = function(tmpDeps) {
+
+        var formatDeps = function (tmpDeps) {
             $scope.deps = [];
             var deps = [];
-            for(var depIndex = 0; depIndex < tmpDeps.length; depIndex++) {
+            $scope.removeSeries();
+
+            var chartSeries = [{
+                    name: 'Aggregation of departments',
+                    colorByPoint: true,
+                    data: []
+                }];
+
+            for (var depIndex = 0; depIndex < tmpDeps.length; depIndex++) {
                 insert(tmpDeps[depIndex].key, deps);
+                var key = (tmpDeps[depIndex].hasOwnProperty('key_as_string')) ? tmpDeps[depIndex].key_as_string : tmpDeps[depIndex].key.toString();
+                chartSeries[0].data.push({
+                    name: key,
+                    y: tmpDeps[depIndex].doc_count,
+                    drilldown: key
+                });
             }
             $scope.deps = deps;
+            $scope.addNewSeries(chartSeries);
+
+        };
+
+        //Add serie as drilldown dynamically
+        $scope.addDrilldown = function (e) {
+            if (e && e.point && !e.seriesOptions) {
+                var drilldown = e.point.drilldown;
+                if (!drilldown) {
+                    return;
+                } else {
+                    var request = 'conservatory_index/conservatories/_search?source={"aggs":{"dep_cp":{"filter":{"term":{"fields.dep":"' + drilldown + '"}},"aggs":{"_res":{"terms":{"field":"fields.cp"}}}}}}';
+
+                    esServ.getData(request, {
+                        then: function (response) {
+                            var drillData = response.data.aggregations.dep_cp._res.buckets;
+                            var chart = $('#containerDrill').highcharts();
+                            var series = {
+                                name: 'Aggregation of postcodes filtered by department: ' + e.point.drilldown,
+                                data: []
+                            };
+                            chart.showLoading();
+
+                            for (var ind = 0; ind < drillData.length; ind++) {
+                                var key = (drillData[ind].hasOwnProperty('key_as_string')) ? drillData[ind].key_as_string : drillData[ind].key.toString();
+                                series.data.push({
+                                    name: key,
+                                    y: drillData[ind].doc_count
+                                });
+                            }
+
+                            $timeout(function () {
+                                chart.hideLoading();
+                                chart.addSeriesAsDrilldown(e.point, series);
+                                chart.redraw();
+                            }, 500);
+                        },
+                        catch : function () {
+                        }
+                    });
+                }
+            }
         };
 
         loadDeps("fields.dep");
