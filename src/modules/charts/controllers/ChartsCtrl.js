69a406605b90d4664c1163ae80ee2266b867bf7e
lili1725 <alice.rimassa@gmail.com>
2016-08-21 19:06:18 +0200

    Translating in english, started charts

--- src/modules/charts/controllers/ChartsCtrl.js
+++ src/modules/charts/controllers/ChartsCtrl.js
@@ -1,6 +1,38 @@
 'use strict';
 
-angular.module('basics').controller('ChartsCtrl', ['$scope', function($scope) {
+angular.module('basics').controller('ChartsCtrl', ['$scope', 'esServ', function ($scope, esServ) {
 
+        $scope.type = 'pie';
+        $scope.dep = 0;
 
-}]);
+        $scope.chooseDep = "Select a department";
+        $scope.deps = [];
+
+        var loadDeps = function (fieldName) {
+            var request = 'conservatory_index/conservatories/_search?source={"size":0,"aggs":{"_res":{"terms":{"field":"' + fieldName + '","size":0}}}}&pretty=true';
+
+            esServ.getData(request, {
+                then: function (response) {
+                    if (response.data && response.data.hasOwnProperty("aggregations")) {
+                        var data = response.data.aggregations._res.buckets || [];
+                        formatDeps(data);
+                    }
+                },
+                catch : function () {
+
+                }
+            });
+        };
+        
+        var formatDeps = function(tmpDeps) {
+            $scope.deps = [];
+            var deps = [];
+            for(var depIndex = 0; depIndex < tmpDeps.length; depIndex++) {
+                insert(tmpDeps[depIndex].key, deps);
+            }
+            $scope.deps = deps;
+        };
+
+        loadDeps("fields.dep");
+
+    }]);
