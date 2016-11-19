cc17f90ba8ed0a037f75759d3ecaf59e8db29058
Vincent Boutour <bob@vibioh.fr>
2016-11-19 18:21:27 +0100

    Upgrading deployment

--- deploy.sh
+++ deploy.sh
@@ -10,21 +10,57 @@ function readVariableIfRequired() {
   fi
 }
 
+function docker-clean() {
+  imagesToClean=`docker images --filter dangling=true -q 2>/dev/null`
+
+  if [ ! -z "${imagesToClean}" ]; then
+    docker rmi ${imagesToClean} 
+  fi
+}
+
 function docker-compose-deploy() {
   PROJECT_NAME=${1}
   readVariableIfRequired "PROJECT_NAME"
 
   DOMAIN=${2}
   readVariableIfRequired "DOMAIN"
-
   export DOMAIN=${DOMAIN}
-  
+
   docker-compose -p ${PROJECT_NAME} pull
-  docker-compose -p ${PROJECT_NAME} stop
-  docker-compose -p ${PROJECT_NAME} rm -f -v
-  docker-compose -p ${PROJECT_NAME} up -d --force-recreate
+  docker-compose -p ${PROJECT_NAME} up -d
+  docker-clean
+}
+
+function docker-compose-hot-deploy() {
+  PROJECT_NAME=${1}
+  readVariableIfRequired "PROJECT_NAME"
+
+  DOMAIN=${2}
+  readVariableIfRequired "DOMAIN"
+  export DOMAIN=${DOMAIN}
+
+  services=`docker-compose -p ${PROJECT_NAME} ps | awk '{if (NR > 2) {print $1}}'`
+
+  docker-compose -p ${PROJECT_NAME} pull
+  matchPattern=${PROJECT_NAME}'_(.*?)_[0-9]+'
+
+  for service in ${services}; do
+    if [[ ${service} =~ ${matchPattern} ]]; then
+      docker-compose -p ${PROJECT_NAME} scale ${BASH_REMATCH[1]}=2
+    fi
+  done
+
+  docker-compose -p ${PROJECT_NAME} up -d
+
+  echo "Waiting 5 seconds to start..."
+  sleep 5
+
+  if [ ! -z "${services}" ]; then
+    docker stop ${services}
+    docker rm -f -v ${services}
+  fi
   
-  docker rmi `docker images --filter dangling=true -q 2>/dev/null` 2>/dev/null
+  docker-clean
 }
 
 export PATH=${PATH}:/opt/bin
@@ -38,5 +74,13 @@ readVariableIfRequired "PROJECT_URL"
 rm -rf ${PROJECT_NAME}
 git clone ${PROJECT_URL} ${PROJECT_NAME}
 cd ${PROJECT_NAME}
-docker-compose-deploy ${PROJECT_NAME} ${3}
 
+export DOMAIN=${3}
+
+if [ `docker-compose -p ${PROJECT_NAME} ps | awk '{if (NR > 2) {print}}' | wc -l` -eq 0 ]; then
+  echo "Deploying new stack"
+  docker-compose-deploy ${PROJECT_NAME} ${3}
+else
+  echo "Hot deploying stack"
+  docker-compose-hot-deploy ${PROJECT_NAME} ${3}
+fi
