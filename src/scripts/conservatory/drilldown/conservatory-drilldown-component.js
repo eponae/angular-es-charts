f93a438505f2e1bd8d5fae75a226a1cafddebfb8
Alice Rimassa <alice@eponae.fr>
2017-11-11 23:45:06 +0100

    Worked on drilldown

--- src/scripts/conservatory/drilldown/conservatory-drilldown-component.js
+++ src/scripts/conservatory/drilldown/conservatory-drilldown-component.js
@@ -1,10 +1,11 @@
-import zingchart from '../../../../node_modules/zingchart/client/zingchart.min.js';
+import zingchart from '../../../../node_modules/zingchart/index.js';
 import conservatoryDrilldownTemplate from './conservatory-drilldown.html';
 
 class ConservatoryDrilldownController {
   constructor(conservatoryService, errorService) {
     this.conservatoryService = conservatoryService;
     this.errorService = errorService;
+    this.drillDownDataStructure = {};
     this.chartData = {
       type: 'hbar',
       title: {
@@ -28,7 +29,7 @@ class ConservatoryDrilldownController {
     };
   }
 
-  formatData(results) {
+  formatDepartments(results) {
     let series = [];
 
     const resultsKeys = Object.keys(results);
@@ -37,22 +38,55 @@ class ConservatoryDrilldownController {
       series.push({
         values: [results[key]],
         text: 'Dep ' + key.toString(),
-        'data-id': key
+        'data-id': key.toString()
       });
     });
 
     this.chartData.series = series;
-    this.chartData.title.text = resultsKeys.length + ' departments';
+    this.chartData.title.text = resultsKeys.length + ' départements';
+  }
+
+  setDrilldownData(dataId) {
+    this.chartData.title.text = 'Département ' + dataId;
+    zingchart.exec('myChart', 'setseriesdata', {
+      data: this.drillDownDataStructure[dataId].series
+    });
+  }
+
+  formatPostalCodes(results, dataId) {
+    let series = [];
+
+    const resultsKeys = Object.keys(results);
+
+    resultsKeys.forEach((key) => {
+      series.push({
+        values: [results[key]],
+        text: key.toString()
+      });
+    });
+
+    this.drillDownDataStructure[dataId] = { series: series };
+    this.setDrilldownData(dataId);
   }
 
   $onInit() {
     this.conservatoryService.getAggregateByDepartment()
       .then((data) => {
-        this.formatData(data);
-        zingchart.node_click = () => {
-        };
+        this.formatDepartments(data);
       })
       .catch(() => this.errorService.showSimpleToast('Une erreur est survenue.'));
+
+    zingchart.node_click = (p) => {
+      const dataId = p['data-id'];
+      if (this.drillDownDataStructure[dataId]) {
+        this.setDrilldownData(dataId);
+      } else {
+        this.conservatoryService.getAggregateByZipForADepartment(p['data-id'])
+          .then((aggregatedData) => {
+            this.formatPostalCodes(aggregatedData, dataId);
+          });
+      }
+    };
   }
 }
 
